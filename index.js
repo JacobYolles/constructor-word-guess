0d4040a8a32efc11d09c1cea2db7e06027410687
JacobYolles <jacob.yolles@ucdenver.edu>
2018-11-02 13:41:59 -0600

    added code and a gitignore

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var word = require("./word.js")
+// Allows the file to require the word.js file.
+var Word = require("./Word.js");
+// Let the game have access to the inquirer package.
+var inquirer = require("inquirer");
+// giving the game some color using this package.
+var clc = require('cli-color');
 
-var wordlist = []
+// Creates the word bank, the pre deterimined list of words that will be picked from for the user to guess at trying to solve.
+var wordList = ["red", "blue", "green", "violet", "purple", "indigo", "yellow", "orange", "maroon", "cyan", "aquamarine"]
+
+// Create the ability for the word to be chosen from the random word list.
+var randomWord; 
+var someWord;
+
+// Create the counters for wins, losses, and guesses remaining.
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 20;
--- word.js
+++ word.js
@@ -10,8 +10,28 @@
 
 // Importing letter.js since it is a reliant file for this file.
 var Letter = require("./Letter");
-
 // create the word constructor:
 var Word = function(myWord) {
-    
+    //have it take the word from the word list!
+    this.myWord = myWord;
+    // create the array of letters representing the random word.
+    this.letters = [];
+    // create the underscores to put on the word based on the amount of letters in the word.
+    this.underscores = [];
+
+    this.splitWord = function() {
+        this.letters = this.myWord.split("")
+        console.log(this.letters)
+
+        // create the ability to determine the amount of underscores needed
+        numberUnderscoresNeeded = this.letters.length
+
+        console.log(this.underscores.join(""))
+    }
+    this.generateLetters = function() {
+
+
+    }
 }
+
+module.exports = Word
